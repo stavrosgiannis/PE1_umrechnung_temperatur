b94c614e6da2e6870c7a92a7bd1f9937d3e4f69c
Stav ros <hackerone1@web.de>
2019-10-09 14:18:11 +0200

    fix for comments

--- PE1_umrechnung_temperatur/main.c
+++ PE1_umrechnung_temperatur/main.c
@@ -1,62 +1,86 @@
-﻿//	____________________________________
-// |Aufgabe 1: Temperaturrechner		|
-// |PE1									|
-// |Author: Stavros Giannis				|
-// |Datum 08/10/2019					|
-//	‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+﻿/*
+	____________________________________
+	|Aufgabe 1: Temperaturrechner		|
+	|PE1								|
+	|Author: Stavros Giannis			|
+	|Datum 08/10/2019					|
+	‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+*/
 
-//Warnungen der "C Run-time Library" werden unterdrückt
-//CRT ist Bestandteil der C/C++ Standardbibliothek "stdlib.h"
+/*
+Warnungen der "C Run-time Library" werden unterdrückt.
+CRT ist Bestandteil der C/C++ Standardbibliothek "stdlib.h".
+#define sind Makros, die bedingt definiert werden.
 #define _CRT_SECURE_NO_WARNINGS 1
+*/
 
-//Die Standardbibliothek "Standardlibrary" wird inkludiert
+//Die Standardbibliothek "Standardlibrary" wird inkludiert.
 #include <stdlib.h>
-//Die Standardbibliothek "Standard [i]nput [o]utput" wird inkludiert
+//Die Standardbibliothek "Standard [i]nput [o]utput" wird inkludiert.
 #include <stdio.h>
 
-//Die Hauptfunktion "main()" wird beim Start des Programms initiiert:
-//Die Funktion 'int main()' ist equivalent zu 'int main(void)'.
-//Beide benötigen keine Argumente als Parameter zum ausführen.
-//Funktionen werden in der Informatik gebraucht,
-//damit von einer Softwareanwendung etwas eindeutig berechnet werden kann.
+/*
+Die Startfunktion "main()" wird beim Start des Programms initiiert:
+Die Funktion 'int main()' ist equivalent zu 'int main(void)'.
+Beide benötigen keine Argumente als Parameter zum ausführen.
+Funktionen werden in der Informatik gebraucht,
+damit von einer Softwareanwendung etwas eindeutig berechnet werden kann.
+*/
 int main()
 {
-	//	__________________
-	// |float	|32Bits   |
-	// |integer	|16/32Bits|		<--		Ein paar Wichtige Datentypen und deren Größe in Bits.
-	// |char	|8Bits	  |				Jeder Datentyp besitzt einen einzigartigen Anwendungsbereich.
-	// |double	|64Bits	  |
-	//  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-	//
-	//Es werden zwei Variablen des Datentyps "float" deklariert.
-	//Der Datentyp float hat einen Wertebereich von +/- 10^38
-	//Eine Variable besteht aus:
-	//DATENTYP[float]		[NAME1,NAME2,..]			(optional[=VALUE])
+	/*
+		__________________
+		|float	|32Bits   |
+		|integer|16/32Bits|		<--		Ein paar Wichtige Datentypen und deren Größe in Bits.
+		|char	|8Bits	  |				Jeder Datentyp besitzt einen einzigartigen Anwendungsbereich.
+		|double	|64Bits	  |
+		‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+
+	Es werden zwei Variablen des Datentyps "float" deklariert.
+	Der Datentyp float hat einen Wertebereich von +/- 10^38
+	Eine Variable besteht aus:
+	DATENTYP[float]		[NAME1,NAME2,..]			(optional[=VALUE])*/
 	float					tempIn, tempOut;					//= 5;
 
-	//Es werden zwei Variablen des Datentyps "integer" deklariert.
-	//Auf der nächsten Zeile wird eine Variable des Datentyps integer separat dazu deklariert.
-	//Der Datentyp integer hat einen Wertebereich von -32768 bis 32767 bei 16 Bit
+	/*Es werden zwei Variablen des Datentyps "integer" deklariert.
+	Auf der nächsten Zeile wird eine Variable des Datentyps integer separat dazu deklariert.
+	Der Datentyp integer hat einen Wertebereich von -32768 bis 32767 bei 16 Bit*/
 	int unitIn, unitOut;
 	int desiredConversion;
 
-	//Es wird eine Variable des Datentyps "char" deklariert.
-	//Der Datentyp char hat einen Wertebereich von -128 bis 127
+	/*Es wird eine Variable des Datentyps "char" deklariert.
+	Der Datentyp char hat einen Wertebereich von -128 bis 127*/
 	char response;
 
-	//[print][formatted] ist eine Ausgabefunktion.
-	//Die Funktion nimmt Texte mit Formatierungshinweisen als Parameter.
-	//"\n" gehört zu den "Escape sequences", welch den Character "Newline" repräsentiert.
-	//Die Zeile gibt den folgenden text aus:
-	//	\n
-	//	\n
-	//	Programm zur Umrechnung von Temperaturen
-	//	\n
+	/*
+	[print][formatted] ist eine Ausgabefunktion.
+	Die Funktion nimmt Texte mit Formatierungshinweisen als Parameter.
+	"\n" gehört zu den "Escape sequences", welch den Character "Newline" repräsentiert.
+	Die Zeile gibt den folgenden text aus:
+		\n											<--- "\n" wird nicht angezeigt, jediglich nur eine neue Zeile
+		\n
+		Programm zur Umrechnung von Temperaturen
+		\n
+	*/
 	printf("\n\nProgramm zur Umrechnung von Temperaturen\n");
-	//Es wird eine reihe von Strichen ausgegeben und am Ende noch eine Newline hinzugefügt.
+
+	//Es wird eine reihe von Strichen ausgegeben und am Ende noch eine neue Zeile hinzugefügt.
 	printf("----------------------------------------\n");
 
-	//Hwefwef
+	/*
+	In den folgenden Zeilen wird eine do-while Schleife ausgeführt.
+	Die kann man wie folgt verstehen:
+
+			do {
+				Anweisungen
+			} while (Prüfe ob Bedingung erfüllt bzw. auf true oder false) {
+				Anweisungen
+				Veränderung des Anfangswertes
+			}
+
+	Bei der do-While Schleife wird die Bedingung erst nach dem ersten Schleifendurchlauf geprüft.
+	Es handelt sich um eine Bedingung für die Widerholung von Anweisungen.
+	*/
 	do {
 		printf("\nAus welchem Einheitensystem soll umgerechnet werden?\n\n");
 		printf("1: grad C     2: grad F     3: K\n\n");
