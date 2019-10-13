c5f22807972fffde54533617cc24c306c08de40b
Stavros <stavros.g@web.de>
2019-10-13 23:20:25 +0200

    bis case

--- PE1_umrechnung_temperatur/main.c
+++ PE1_umrechnung_temperatur/main.c
@@ -82,42 +82,86 @@ int main()
 	Es handelt sich um eine Bedingung für die Wiederholung von Anweisungen.
 	*/
 	do {
+		//Die CLI gibt einen Text aus mit entsprechenden Zeilen abständen
 		printf("\nAus welchem Einheitensystem soll umgerechnet werden?\n\n");
 		printf("1: grad C     2: grad F     3: K\n\n");
 		printf("Ihre Wahl: ");
+		/*
+		Die Funktion scanf bietet die Möglichkeiten die Eingabe des Nutzers auszulesen
+		In dem Fall wird der Integer, also die eingebene Zahl, in der Variable "unitIn" gespeichert.
+		
+		printf hat die Kompabilität Formatierungszeichen zu benutzen:
+				%d %i	Decimal signed integer.
+				%o	Octal integer.
+				%x %X	Hex integer.
+				%u	Unsigned integer.
+				%c	Character.
+				%s	String.	siehe unten.
+				%f	double
+				%e %E	double.
+				%g %G	double.
+				%p	zeiger.
+				%n	Number of characters written by this printf.
+					No argument expected.
+				%%	%. No argument expected.
+
+		*/
+		//Die Variable "unitIn" repräsentiert "%i".
 		scanf("%i", &unitIn);
+		//Die Schleife folgt erst dann fort, wenn die Eingabe des Nutzers nicht nur eine "neue Zeile" ist.
 		while (getchar() != '\n');
 
+		//Hier wird wieder mit printf etwas Ausgegeben
 		printf("\nBitte geben Sie die umzurechnende Temperatur an: ");
+		//Dieses mal wird die Eingabe des Nutzers als float in die Variable "tempIn" gespeichert.
+		//Dies ermöglicht eine Eingabe als Dezimalzahl, welche eine Temperatur auch sein kann.
 		scanf("%f", &tempIn);
+		//Die Schleife folgt erst dann fort, wenn die Eingabe des Nutzers nicht nur eine "neue Zeile" ist.
 		while (getchar() != '\n');
 
+		//Hier wird die Ausgabe so angepasst, dass der Nutzer sich mit der Eingabe 1 für grad Celsius, 2 für grad Fahrenheit
+		//und 3 für Kelvin entscheiden kann.
 		printf("\nIn welches Einheitensystem soll umgerechnet werden?\n\n");
 		printf("1: grad C     2: grad F     3: K\n\n");
 
 		printf("Ihre Wahl: ");
+		//Der Input des Nutzers wird in die Variabel "unitOut" gesetzt.
 		scanf("%i", &unitOut);
+		//Die Schleife folgt erst dann fort, wenn die Eingabe des Nutzers nicht nur eine "neue Zeile" ist.
 		while (getchar() != '\n');
 
+		//die Variabel "desireConversion" bildet sich wenn man den Wert von "unitIn" mit der 10 multipliziert und noch
+		//den Wert der Variabel "unitOut" addiert.
 		desiredConversion = unitIn * 10 + unitOut;
+
+		//Das Switch-Case Element wird meist als eine Alternative zu den if-Abfragen benutzt. Es ist unpraktisch wenn man
+		//viele Abfragen von Bedingungen mit If-Anweisungen erfüllen will.
+		//Die Variable hinter switch(desiredConversion) gibt einen Rückgabewert einer Funktion oder einer Variable an. 
 		switch (desiredConversion)
 		{
-		case 12: tempOut = tempIn * 1.8 + 32.0;
+		//IM FALLE IST 12: Falls der Rückgabewert der Variable 12 ist, wird dieser case-Block ausgeführt.
+		case 12: 
+			tempOut = tempIn * 1.8 + 32.0;
 			printf("\n\n%.2fgrad C = %.2fgrad F\n\n", tempIn, tempOut);
 			break;
-		case 13: tempOut = tempIn + 273.15;
+		case 13: 
+			tempOut = tempIn + 273.15;
 			printf("\n\n%.2fgrad C = %.2fK\n\n", tempIn, tempOut);
 			break;
-		case 21: tempOut = (tempIn + 32.0) * 5.0 / 9.0;
+		case 21: 
+			tempOut = (tempIn + 32.0) * 5.0 / 9.0;
 			printf("\n\n%.2fgrad F = %.2fgrad C\n\n", tempIn, tempOut);
 			break;
-		case 23: tempOut = (tempIn + 459.67) * 5.0 / 9.0;
+		case 23: 
+			tempOut = (tempIn + 459.67) * 5.0 / 9.0;
 			printf("\n\n%.2fgrad F = %.2fK\n\n", tempIn, tempOut);
 			break;
-		case 31: tempOut = tempIn + 273.15;
+		case 31: 
+			tempOut = tempIn + 273.15;
 			printf("\n\n%.2fK = %.2fgrad C\n\n", tempIn, tempOut);
 			break;
-		case 32: tempOut = tempIn * 1.8 - 459.67;
+		case 32: 
+			tempOut = tempIn * 1.8 - 459.67;
 			printf("\n\n%.2fK = %.2fgrad F\n\n", tempIn, tempOut);
 		}
 		printf("\n\nerneute Berechnung? (j/n)");
